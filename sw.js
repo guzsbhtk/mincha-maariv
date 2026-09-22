960b88ae69818bad122b7d384d91302e13132184
oz daniel <116812513+guzsbhtk@users.noreply.github.com>
2026-09-22 06:55:07 -0400

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tefilot-tshpz-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+const CACHE = "tefilot-tshpz-v2";
+const ASSETS = ["./", "./index.html", "./zmanim-data.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
